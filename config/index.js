3de272760613ad69db73cf07423b9a555609929d
minaamfasihi <minaam.fasihi@gmail.com>
2018-11-19 00:48:34 +0500

    initial commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+var configValues = require('./config');
+module.exports = {
+    getDbConnectionString: function() {
+        return 'mongodb://localhost:27017/NodeTodo';
+    }
+};
---
+++ controllers/setupController.js
@@ -0,0 +1,23 @@
+var Todos = require('../models/todoModel');
+
+module.exports = function(app) {
+    app.get('/api/setupTodos', function(req, res) {
+        var starterTodos = [
+            {
+                username: 'test',
+                todo: 'Buy milk',
+                isDone: false,
+                hasAttachment: false
+            },
+            {
+                username: 'test',
+                todo: 'Buy eggs',
+                isDone: false,
+                hasAttachment: false
+            }
+        ];
+        Todos.create(starterTodos, function(err, results) {
+            res.send(results);
+        });
+    });
+}
